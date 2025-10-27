b00b09e9996c573e7646291b4cbde7687f48fe80
Chan Nyein Aung <thesmartgooners1@gmail.com>
2025-10-27 17:49:09 +0700

    Update main.js

--- main.js
+++ main.js
@@ -2,14 +2,14 @@ const menuBtn = document.getElementById("menu-btn");
 const navLinks = document.getElementById("nav-links");
 const menuBtnIcon = menuBtn.querySelector("i");
 
-menuBtn.addEventListener("click", (e) => {
+menuBtn.addEventListener("click", () => {
   navLinks.classList.toggle("open");
 
   const isOpen = navLinks.classList.contains("open");
   menuBtnIcon.setAttribute("class", isOpen ? "ri-close-line" : "ri-menu-line");
 });
 
-navLinks.addEventListener("click", (e) => {
+navLinks.addEventListener("click", () => {
   navLinks.classList.remove("open");
   menuBtnIcon.setAttribute("class", "ri-menu-line");
 });
@@ -21,7 +21,7 @@ const scrollRevealOption = {
 };
 
 // header container
-ScrollReveal().reveal(".header__container .section__subheader", {
+ScrollReveal().reveal(".header__container p", {
   ...scrollRevealOption,
 });
 
@@ -30,9 +30,30 @@ ScrollReveal().reveal(".header__container h1", {
   delay: 500,
 });
 
-ScrollReveal().reveal(".header__container .btn", {
+// about container
+ScrollReveal().reveal(".about__image img", {
+  ...scrollRevealOption,
+  origin: "left",
+});
+
+ScrollReveal().reveal(".about__content .section__subheader", {
+  ...scrollRevealOption,
+  delay: 500,
+});
+
+ScrollReveal().reveal(".about__content .section__header", {
   ...scrollRevealOption,
   delay: 1000,
+});
+
+ScrollReveal().reveal(".about__content .section__description", {
+  ...scrollRevealOption,
+  delay: 1500,
+});
+
+ScrollReveal().reveal(".about__btn", {
+  ...scrollRevealOption,
+  delay: 2000,
 });
 
 // room container
@@ -41,14 +62,9 @@ ScrollReveal().reveal(".room__card", {
   interval: 500,
 });
 
-// feature container
-ScrollReveal().reveal(".feature__card", {
+// service container
+ScrollReveal().reveal(".service__list li", {
   ...scrollRevealOption,
   interval: 500,
-});
-
-// news container
-ScrollReveal().reveal(".news__card", {
-  ...scrollRevealOption,
-  interval: 500,
+  origin: "right",
 });
